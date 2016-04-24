a446d3b9e145eafbcbf6b79fc202dde2036d72ee
ozdreams <ozdreamsks@gmail.com>
2016-04-24 16:12:21 -0500

    Initial Commit

---
+++ PhysicsPotentialEnergy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhysicsPotentialEnergy
+//
+//  Created by austin crane on 4/24/16.
+//  Copyright © 2016 Coffee House. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
